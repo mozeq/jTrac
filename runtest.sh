16db665950973deab6c193286b5d42ce26e89e16
Jiri Moskovcak <jmoskovc@redhat.com>
2012-10-24 18:59:41 +0200

    added script to run the test

---
+++ runtest.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+java -classpath "bin:/usr/share/java/jsoup.jar:jars/commons-logging-1.1.jar:jars/ws-commons-util-1.0.2.jar:jars/xmlrpc-client-3.1.3.jar:jars/xmlrpc-common-3.1.3.jar:jars/xmlrpc-server-3.1.3.jar" "org.mozeq.Trac.jTracTest"
